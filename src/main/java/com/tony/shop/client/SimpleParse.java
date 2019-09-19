6b27c2e91e8cd961c8088c13f76ad0a2b8e7c44d
tony <no@no.com>
2019-09-19 23:06:06 +0800

    remove fucking multipart parse

--- src/main/java/com/tony/shop/client/SimpleParse.java
+++ src/main/java/com/tony/shop/client/SimpleParse.java
@@ -22,46 +22,5 @@ public class SimpleParse {
 			"null";
 
 	public static void main(String[] args) {
-		List<String> fileString = new ArrayList<>();
-
-		String[] fs = FS.split("\n");
-		String boundary = fs[0];
-		int iscont = 0;
-		StringBuffer sbuf = new StringBuffer(256);
-
-		for (int i = 0; i < fs.length; i++) {
-			String line = fs[i];
-			if (line.indexOf(boundary) == -1) {
-				// check the first three content-type
-				if (iscont == 0) {
-					i += 2;   // "Content-Disposition" && "Content-Type"
-					if (fs[i].indexOf("Content-Transfer-Encoding") > -1) {
-						i++;  // skip line
-					}
-					iscont = 1;
-				} else {
-					if (i + 1 < fs.length && fs[i + 1].indexOf(boundary) == -1) {
-						sbuf.append(fs[i]);
-						sbuf.append("\n");
-					}
-				}
-			} else {
-				// another boundary
-				if (sbuf.length() == 0)
-					continue;
-
-				fileString.add(sbuf.toString());
-				sbuf = new StringBuffer(256);
-				iscont = 0;
-
-				if (fs[i+1].equals("null"))
-					break;
-			}
-		}
-
-		if (sbuf.length() > 0)
-			fileString.add(sbuf.toString());
-
-		int asm3 = 0;
 	}
 }
--- src/main/java/com/tony/shop/service/impl/FileService.java
+++ src/main/java/com/tony/shop/service/impl/FileService.java
@@ -1,78 +1,60 @@
 package com.tony.shop.service.impl;
 
-import org.jboss.resteasy.plugins.providers.multipart.InputPart;
 import org.jboss.resteasy.plugins.providers.multipart.MultipartFormDataInput;
 
 import javax.ws.rs.*;
-import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
 
 @Path("/file")
 public class FileService {
-	private static final String BASEDIR = "/";
+    private static final String BASEDIR = "/";
 
-	/**
-	 * @param path
-	 * @return FileInputStream
-	 * curl http://localhost:8080/services/file/tmp/1.tmp
-	 */
-	@GET
-	@Path("stream/{filepath: .*}")
-	@Produces("text/plain")
-	public InputStream getFileStream(@PathParam("filepath") String path) {
-		FileInputStream file = null;
-		try {
-			file = new FileInputStream(BASEDIR + path);
-		} catch (Exception e) {
-			System.err.println(e.getMessage());
-		}
+    /**
+     * @param path
+     * @return FileInputStream
+     * curl http://localhost:8080/services/file/tmp/1.tmp
+     */
+    @GET
+    @Path("stream/{filepath: .*}")
+    @Produces("text/plain")
+    public InputStream getFileStream(@PathParam("filepath") String path) {
+        FileInputStream file = null;
+        try {
+            file = new FileInputStream(BASEDIR + path);
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
 
-		return file;
-	}
+        return file;
+    }
 
-	/**
-	 * @param path
-	 * @return File
-	 * curl http://localhost:8080/services/file/tmp/1.tmp
-	 */
-	@GET
-	@Path("{filepath: .*}")
-	@Produces("text/plain")
-	public File getFile(@PathParam("filepath") String path) {
-		File file = null;
-		try {
-			file = new File(BASEDIR + path);
-		} catch (Exception e) {
-			System.err.println(e.getMessage());
-		}
+    /**
+     * @param path
+     * @return File
+     * curl http://localhost:8080/services/file/tmp/1.tmp
+     */
+    @GET
+    @Path("{filepath: .*}")
+    @Produces("text/plain")
+    public File getFile(@PathParam("filepath") String path) {
+        File file = null;
+        try {
+            file = new File(BASEDIR + path);
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
 
-		return file;
-	}
+        return file;
+    }
 
 
-	/**
-	 *
-	 * @param file, the whole filepart, including "--------XXXXXX content-type"
-	 * @return
-	 *
-	 */
-	@Path("post")
-	@POST
-	@Produces("text/plain")
-	public String postFile(File file) {
-		StringBuffer sb = new StringBuffer(1024);
-		try {
-			FileReader fr = new FileReader(file);
-			LineNumberReader lnr = new LineNumberReader(fr);
-			String line = null;
-			do {
-				line = lnr.readLine();
-				sb.append(line);
-				sb.append("\n");
-			} while (line != null);
-		} catch (Exception e) {
-
-		}
-
-		return sb.toString();
-	}
+    @Path("post")
+    @POST
+    @Produces("text/plain")
+    public String postFile(MultipartFormDataInput file) {
+        file.getFormData();
+        return "ok";
+    }
 }
