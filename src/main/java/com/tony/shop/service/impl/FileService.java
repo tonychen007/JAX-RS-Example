6a98d6cc89ec145865018b4d623ca2fe3e639fc5
tony <no@no.com>
2019-09-21 17:09:35 +0800

    add form-data and rawdata post

--- src/main/java/com/tony/shop/service/impl/FileService.java
+++ src/main/java/com/tony/shop/service/impl/FileService.java
@@ -3,6 +3,7 @@ package com.tony.shop.service.impl;
 import org.jboss.resteasy.plugins.providers.multipart.MultipartFormDataInput;
 
 import javax.ws.rs.*;
+import javax.ws.rs.core.MultivaluedMap;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
@@ -57,4 +58,22 @@ public class FileService {
         file.getFormData();
         return "ok";
     }
+
+    @Path("rawdata")
+    @POST
+    @Produces("text/plain")
+    // curl -d "abc" http://....
+	public String postRawdata(byte[] bys) {
+    	return "bys";
+    }
+
+    @Path("formdata")
+	@POST
+    @Consumes("application/x-www-form-urlencoded")
+    @Produces("application/x-www-form-urlencoded")
+    // curl -d "first=tony&last=chen" http://....
+    public MultivaluedMap<String,String> post(MultivaluedMap<String,String> data) {
+    	return data;
+    }
+
 }
